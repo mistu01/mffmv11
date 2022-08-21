33896135f7c713f0ad5146a2e8079bf263458bfc
Mistu Marsh <dreamzofabird@gmail.com>
2022-08-22 01:08:24 +0600

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -27,12 +27,10 @@ MFFM=/sdcard/MFFM
 [ ! -d $MFFM ] && echo 'MFFM folder and resourecs not found..' && mkdir -p $MFFM
 
 mffmex(){
-    f='Thin.ttf ThinItalic.ttf Light.ttf LightItalic.ttf Regular.ttf Italic.ttf Medium.ttf MediumItalic.ttf Bold.ttf BoldItalic.ttf Black.ttf BlackItalic.ttf'
-	for i in $f; do
-	    if [ ! -f "$FONTDIR/$i" ]; then
-		    cp $MFFM/$i $FONTDIR
-		fi
-	done
+    if [ ! -f $FONTDR/Regular.ttf ]; then
+	    set Black BlackItalic Bold BoldItalic Medium MediumItalic Regular Italic Light LightItalic Thin ThinItalic
+	    for i do cp $MFFM/$i.ttf $FONTDIR/$i.ttf; done
+	fi
     if [ ! -f $FONTDIR/Beng*.ttf ] && [ ! -f $FONTDIR/Beng*.zip ] && [ ! -f $MFFM/Beng*.ttf ] && [ -f $MFFM/Beng*.zip ]; then
         cp $MFFM/Beng*.zip $FONTDIR
     fi
